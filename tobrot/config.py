afd28e55e93ad3db7c66d859042c0d9755e67382
deaduserbot <70665915+deaduserbot@users.noreply.github.com>
2020-12-16 20:36:11 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -10,7 +10,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = "1494936760:AAEBU-KROFifr5TrjqdPimR7-12RoaVeXvE"
+    TG_BOT_TOKEN = "1494936760:AAFYAwIIYNL-qYQ4pty9lgQsv-dXUiXs_t8"
     # The Telegram API things
     APP_ID = 1640301
     API_HASH = "a4041a10f706bc95285393d871357007"
